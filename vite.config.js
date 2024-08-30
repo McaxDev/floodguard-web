45798334ff821d2dd1e3b91a780a33d90fe3157e
Nerakolo <nerakolo@outlook.com>
2024-08-30 22:50:46 +0800

    新增依赖

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue({
@@ -13,6 +12,7 @@ export default defineConfig({
         }
       }
     }),
+    // mdPlugin(),
   ],
   resolve: {
     alias: {
